a20856ff96f4a06084a3b655fa44b31215fd3fbd
James Iturralde <jamesiturralde@SkillDs-MacBook-Pro.local>
2019-09-15 20:22:32 -0600

    Had to lose the switch and revert organization to if statments.

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,154 +1,129 @@
 
 import java.util.Scanner;
 
+
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		double input=0, i=0, dol=0, c=0;
+		double purchase=0, payment=0, i=0;
 
+		cashBack(purchase, payment, i);
+		cashReturned(i);
+//		wollyWorldReceipt(purchase, payment, i);
+//		System.out.println("\n\n" + bills + " Dollar Bills");
+//		System.out.println("\n" + cents + " Coins");
+
+
+	}	
+	
+	public static double cashBack(double purchase, double payment, double i) 
+	{		
 		Scanner kb = new Scanner(System.in);
-
-		System.out.print("Enter Total of Purchase $:");
-		input = kb.nextDouble();
+		System.out.print("Enter Amount of Purchase $");
+		purchase = kb.nextFloat();
+		System.out.print("Enter Amount Paid by Customer $");
+		payment = kb.nextFloat();
 		
-
-		
-		double cust = cashBack(input, i);
-		double bills = dollars(i, dol, c);
-		double cents = cents(c, i);
-
-		System.out.println("Give the following change to the customer $: ");
-
-		System.out.println("Give: " + cust + " as: ");
-		System.out.println(bills + "Dollar Bills");
-		System.out.println(cents + " Coins");
-
-		kb.close();
-
-	}
-	
-	
-	public static double cashBack(double input, double i) 
-	{
-		i = 0;
-		while (input <= 1000.00) 
-		{ 
-			
-			if (input <= 1000.00 && input >= 0.00) 
+		while (purchase <= payment && payment > 0.00) 
+		{ 			
+			if (payment > purchase) 
 			{
-				i = 1000.00 - input;
-//				return i;
-			}
+				System.out.print("\nThe Amount Owed is: $");
 
+				i = payment - purchase;
+				
+				System.out.printf("%.2f", i); 
+				kb.close();
+				break;
+			}
+			else if (payment == purchase) {
+				System.out.println("Exact Change Paid");
+				break;
+			}
+			
 			else 
 			{
-				System.out.println("Enter an amount less than $1000 and higher than $0.00"); 
-				i = input;
+				System.out.println("1st Enter the amount of purchase."); 
+				System.out.println("2nd Enter the amount paid.\n\n");
+				break;
 			}		
 		}
 		return i;
 	}
 	
-	public static double dollars(double i, double dol, double c) 
-	{	
-							
-			switch((int)i) 
-			{
-				case 100:
-					if (i <= 1000.00)
+	public static double cashReturned(double i) 
+	{								
+//bill denominations
+//			     $100:
+					if (i < 1000.00)
 					{
-						dol = i / 100.00;
-						System.out.println("Return: " + dol + " One Hundred Dollor Bills");
+						i = i / 100.00;
+
+						System.out.println("\n\nReturn: " + i + " Hundred Dollor Bills");
 					}							
-				case 50:
-					if (i < 100.00 && i >= 50.00) { 
-						dol = i / 50.00;
-						System.out.println("Return: " + dol + " Twenty Dollor Bills"); 
+//				 $50:
+					else if (i < 100.00 && i >= 50.00) { 
+						i = i / 50.00;
+						System.out.println("\nReturn: " + i + " Twenty Dollor Bills"); 
 					}
-				case 20:
-					if (i < 50.00 && i >= 20.00) { 
-						dol = i / 20.00;
-						System.out.println("Return: " + dol + " Twenty Dollor Bills"); 
+//				 $20:
+					else if (i < 50.00 && i >= 20.00) { 
+						i = i / 20.00;
+						System.out.println("\nReturn: " + i + " Twenty Dollor Bills"); 
 					}
-				case 10:
-					if (i < 20.00 && i >= 10.00){	
-						dol = i / 10.00;
-						System.out.println("Return: " + dol + " Ten Dollor Bills");
+//				 $10:
+					else if (i < 20.00 && i >= 10.00){	
+						i = i / 10.00;
+						System.out.println("\nReturn: " + i + " Ten Dollor Bills");
 					}					
-				case 5:
-					if (i < 10.00 && i >= 5.00){
-						dol = i / 5.00;
-						System.out.println("Return: " + dol + " Twenty Dollor Bills");
+//				 $5:
+					else if (i < 10.00 && i >= 5.00){
+						i = i / 5.00;
+						System.out.println("\nReturn: " + i + " Twenty Dollor Bills");
 					}
-				case 1: 
-					if (i < 5.00 && i >= 1){
-						dol = i / 1.00;
-						System.out.println("Return: " + dol + " One Dollar Bills");
+//				 $1: 
+					else if (i < 5.00 && i >= 1){
+						i = i / 1.00;
+						System.out.println("\nReturn: " + i + " One Dollar Bills");
 					}
-/*				case changeDue:
-					if (i < 1.00){
-						c = i;
-						double cents = cents(c);
-						break;
+//////////////////////////////////////////////////////////////////////////////////////
+					
+// 		      	0.25:
+					else if (i <= 0.99 && i >= 0.25) {
+						i = i / 0.25;
+						System.out.println("\n\nReturn: " + i + " Quarters"); 
 					}
-*/				}	
-			
-			return dol;
-			
-	   }
-
-
-
-
-
-
-	public static double cents(double c, double i) 
-		{
-			
-
-		
-		
-//		For 0.25:
-			if (c <= 0.99 && c >= 0.25) {
-				c = c / 0.25;
-				System.out.println("Return: " + c + " Quarters"); 
-				}
-//		For 0.10:
-			else if (c < 0.25 && c >= 0.10){
-				c = c / 0.10;
-				System.out.println("Return: " + c + " Dimes");
-				}				
-//		For 0.05:
-			else if (c < 0.10 && c >= 0.05){
-				c = c / 0.05;
-				System.out.println("Return: " + c + " Nickels");
-				}
-//		For 0.01: 
-			else if (c < 0.05){
-				c = c / 0.01;
-				System.out.println("Return: " + c + " Pennies");
-				}
-
-			return c;
-		}					
-	
+//			 	0.10:
+					else if (i < 0.25 && i >= 0.10){
+						i = i / 0.10;
+						System.out.println("Return: " + i + " Dimes");
+					}				
+//			 	0.05:
+					else if (i < 0.10 && i >= 0.05){
+						i = i / 0.05;
+						System.out.println("Return: " + i + " Nickels");
+					}
+//				0.01: 
+					else if (i < 0.05 && i > 0.00){
+						i = i / 0.01;
+						System.out.println("\nReturn: " + i + " Pennies");
+					}
+					return i;
+		}						
 }
 
-
-/*	
-	public class wollyworldReceipt(int dol, double c double input, double i)
-	{
-	public static void main( String[] args ) {
+	
+/*	public static double wollyWorldReceipt(double purchase, double payment, double i){
 	System.out.println( "+------------------------+" );
 	System.out.println( "|                        |" );
 	System.out.println( "|      WOLLY WORLD       |" );
 	System.out.println( "| Friday                 |" );
 	System.out.println( "| 2019-09-13  23:31 PM   |" );
 	System.out.println( "|                        |" );
-	System.out.println( "| Purchase = $" + dol + c + " | ");
-	System.out.println( "|  Amount Tendered = $" + input + "| ");
+	System.out.println( "| Purchase = $" + purchase + " | ");
+	System.out.println( "|  Amount Tendered = $" + payment + "| ");
 	System.out.println( "|                        |" );
-	System.out.println( "| Cash Returned = $" + dol + c + " |");
+	System.out.println( "| Cash Returned = $" + i + " |");
 	System.out.println( "|                        |" );
 	System.out.println( "|       THANK YOU        |" );
 	System.out.println( "|         JASON          |" );
