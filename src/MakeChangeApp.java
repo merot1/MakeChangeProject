637d71324f2d5ff03a1dd2d3c6fda8bdddfafc11
James Iturralde <jamesiturralde@SkillDs-MacBook-Pro.local>
2019-09-15 20:42:27 -0600

    progress

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -7,8 +7,8 @@ public class MakeChangeApp {
 	public static void main(String[] args) {
 		double purchase=0, payment=0, i=0;
 
-		cashBack(purchase, payment, i);
-		cashReturned(i);
+		cashTendered(purchase, payment, i);
+//		cashReturned(i);
 //		wollyWorldReceipt(purchase, payment, i);
 //		System.out.println("\n\n" + bills + " Dollar Bills");
 //		System.out.println("\n" + cents + " Coins");
@@ -16,13 +16,13 @@ public class MakeChangeApp {
 
 	}	
 	
-	public static double cashBack(double purchase, double payment, double i) 
+	public static double cashTendered(double purchase, double payment, double i) 
 	{		
 		Scanner kb = new Scanner(System.in);
 		System.out.print("Enter Amount of Purchase $");
-		purchase = kb.nextFloat();
+		purchase = kb.nextDouble();
 		System.out.print("Enter Amount Paid by Customer $");
-		payment = kb.nextFloat();
+		payment = kb.nextDouble();
 		
 		while (purchase <= payment && payment > 0.00) 
 		{ 			
@@ -33,6 +33,7 @@ public class MakeChangeApp {
 				i = payment - purchase;
 				
 				System.out.printf("%.2f", i); 
+				cashReturn(i);
 				kb.close();
 				break;
 			}
@@ -51,62 +52,62 @@ public class MakeChangeApp {
 		return i;
 	}
 	
-	public static double cashReturned(double i) 
+	public static double cashReturn(double i) 
 	{								
 //bill denominations
 //			     $100:
-					if (i < 1000.00)
+					if (i >= 100.00 && i < 1000.00)
 					{
 						i = i / 100.00;
 
-						System.out.println("\n\nReturn: " + i + " Hundred Dollor Bills");
+						System.out.println("\n\nReturn: " + i + " Hundred Dollor Bill");
 					}							
 //				 $50:
 					else if (i < 100.00 && i >= 50.00) { 
 						i = i / 50.00;
-						System.out.println("\nReturn: " + i + " Twenty Dollor Bills"); 
+						System.out.println("\n\nReturn: " + i + " Twenty Dollor Bill"); 
 					}
 //				 $20:
 					else if (i < 50.00 && i >= 20.00) { 
 						i = i / 20.00;
-						System.out.println("\nReturn: " + i + " Twenty Dollor Bills"); 
+						System.out.println("\n\nReturn: " + i + " Twenty Dollor Bill"); 
 					}
 //				 $10:
 					else if (i < 20.00 && i >= 10.00){	
 						i = i / 10.00;
-						System.out.println("\nReturn: " + i + " Ten Dollor Bills");
+						System.out.println("\n\nReturn: " + i + " Ten Dollor Bill");
 					}					
 //				 $5:
 					else if (i < 10.00 && i >= 5.00){
 						i = i / 5.00;
-						System.out.println("\nReturn: " + i + " Twenty Dollor Bills");
+						System.out.println("\n\nReturn: " + i + " Twenty Dollor Bill");
 					}
 //				 $1: 
 					else if (i < 5.00 && i >= 1){
 						i = i / 1.00;
-						System.out.println("\nReturn: " + i + " One Dollar Bills");
+						System.out.println("\n\nReturn: " + i + " One Dollar Bill");
 					}
 //////////////////////////////////////////////////////////////////////////////////////
 					
 // 		      	0.25:
 					else if (i <= 0.99 && i >= 0.25) {
 						i = i / 0.25;
-						System.out.println("\n\nReturn: " + i + " Quarters"); 
+						System.out.println("\n\nReturn: " + i + " Quarter"); 
 					}
 //			 	0.10:
 					else if (i < 0.25 && i >= 0.10){
 						i = i / 0.10;
-						System.out.println("Return: " + i + " Dimes");
+						System.out.println("\n\nReturn: " + i + " Dime");
 					}				
 //			 	0.05:
 					else if (i < 0.10 && i >= 0.05){
 						i = i / 0.05;
-						System.out.println("Return: " + i + " Nickels");
+						System.out.println("\n\nReturn: " + i + " Nickel");
 					}
 //				0.01: 
 					else if (i < 0.05 && i > 0.00){
 						i = i / 0.01;
-						System.out.println("\nReturn: " + i + " Pennies");
+						System.out.println("\n\nReturn: " + i + " Penny");
 					}
 					return i;
 		}						
